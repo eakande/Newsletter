8b22708fa9719c5f9009805f8c2ed3bc131f58dd
eakande <89161472+eakande@users.noreply.github.com>
2022-09-10 22:21:18 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -44,9 +44,15 @@ y = data['Headline Inflation']
 ########## Dataset Split ########
 
 
+X_train = X[X.index < 90]
+y_train = y[y.index < 90]              
+    
+X_test = X[X.index >= 90]    
+y_test = y[y.index >= 90]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-print(X_train.shape,y_train.shape,X_test.shape,y_test.shape)
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# print(X_train.shape,y_train.shape,X_test.shape,y_test.shape)
 
 
 
@@ -217,7 +223,7 @@ from dash_bootstrap_components.themes import CYBORG,PULSE,DARKLY
 db=ExplainerDashboard(explainer, [CustomModelTab, CustomModelTab1, CustomPredictionsTab,
                                CustomPredictionsTab2, CustomPredictionsTab3, CustomPredictionsTab4], 
                         title='Inflation Explainer Dasboard for Nigeria', header_hide_selector=False,
-                        bootstrap=CYBORG)
+                        bootstrap=CYBORG).run()
 
 
 
